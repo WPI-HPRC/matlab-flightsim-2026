cf240efc17c87333a02bf90f5e3fd03635d68ba2
krizzyegg <eganchristopher06@gmail.com>
2025-10-20 20:49:53 -0400

    No more NAN errors

--- Utils/skewSymmetric.m
+++ Utils/skewSymmetric.m
@@ -11,6 +11,6 @@ end
 
 skewMatrix = [0, -1.0 * vec(3), vec(2);
                     vec(3), 0, -1.0 * vec(1);
-                    -1.0 * vec(2), vec(1), 0]
+                    -1.0 * vec(2), vec(1), 0];
 end
 
