2148b08fe3c9bb4d5e6b1de3855900b0a064ad1c
Wing57 <ddariusd007@Gmail.com>
2025-10-29 19:41:21 -0400

    refactor plotnav for posteriorstate

--- Plotting/plotNav.m
+++ Plotting/plotNav.m
@@ -42,16 +42,19 @@ function plotNav(out, kfInds)
     end
 
     % === Navigation State Estimates ===
-    navTime = out.NavBus.x.Time;
-    x_est = out.NavBus.x.Data;
-    P     = out.NavBus.P.Data;
+    % Get data from posterior state output (22x1 state vector)
 
-    q_est  = x_est(1:4, :);
-    gb_est = x_est(kfInds.gyroBias, :);
-    ab_est = x_est(kfInds.accelBias, :);
-    mb_est = x_est(kfInds.magBias, :);
-    pos_est = x_est(kfInds.pos, :);
-    vel_est = x_est(kfInds.vel, :);
+    navTime = out.abhay_priori_state.Time;       
+    x_est = out.abhay_posteriori_state.Data'; 
+    P = out.abhay_posteriori_P.Data;           
+
+    q_est   = x_est(1:4, :);        % Quaternion
+    vel_est = x_est(5:7, :);        % Velocity  
+    pos_est = x_est(8:10, :);       % Position
+    gb_est  = x_est(11:13, :);      % Gyro Bias
+    gps_bias_est = x_est(14:16, :); % GPS Bias 
+    ab_est  = x_est(17:19, :);      % Accel Bias
+    mb_est = x_est(20:21, :);       % Mag Bias
 
     % === Resample Ground Truth ===
     pos_true_resampled = resampleTimeSeries(pos_T_true, truthTime, navTime);
@@ -82,28 +85,30 @@ function plotNav(out, kfInds)
     % === Bias Error ===
     ab_err = ab_est - ICM20948_PARAMS.accel.bias;
     gb_err = gb_est - ICM20948_PARAMS.gyro.bias;
-    mb_err = mb_est - MMC5983_PARAMS.bias;
+    mb_err = mb_est - MMC5983_PARAMS.bias(1:2);
 
-    % === Euler Angles ===
-    % Convert truth quaternions to Euler (ZYX order)
-    quatToEulerZYX = @(q) rad2deg(quat2eul(q', 'ZYX'));  % N x 3
-    eul_true = quatToEulerZYX(q_true_resampled);
+    % === Update kfInds for your 22-state MEKF ===
+    % Define indices for your state vector
+    kfInds_mekf.quat = 1:4;
+    kfInds_mekf.vel = 5:7;
+    kfInds_mekf.pos = 8:10;
+    kfInds_mekf.gyroBias = 11:13;
+    kfInds_mekf.gpsBias = 14:16;  
+    kfInds_mekf.accelBias = 17:19;
+    kfInds_mekf.magBias = 20:21;
     
-    % Get estimated Euler angles from state
-    eul_est = rad2deg(x_est(kfInds.eul, :))';  % [N x 3], convert from rad to deg if stored in rad
-    
-    % Euler error with wrapping
-    eul_error = wrapTo180(eul_true - eul_est);
-   
-
     % === Plotting ===
-    % plotWithCovariance(navTime, q_err, P, kfInds.quat, 'Quaternion Error', {'q_w', 'q_x', 'q_y', 'q_z'});
-    plotWithCovariance(navTime, pos_error, P, kfInds.pos, 'Position Error (m)', {'North', 'East', 'Down'});
-    plotWithCovariance(navTime, vel_err, P, kfInds.vel, 'Velocity Error (m/s)', {'V_N', 'V_E', 'V_D'});
-    plotWithCovariance(navTime, gb_err, P, kfInds.gyroBias, 'Gyro Bias Estimation (rad/s)', {'X', 'Y', 'Z'});
-    plotWithCovariance(navTime, ab_err, P, kfInds.accelBias, 'Accel Bias Estimation (m/s^2)', {'X', 'Y', 'Z'});
-    plotWithCovariance(navTime, mb_err, P, kfInds.magBias, 'Mag Bias Estimation (uT)', {'X', 'Y', 'Z'});
-    plotWithCovariance(navTime, eul_error, P, kfInds.eul, 'Euler Angle Error (deg)', {'Yaw', 'Pitch', 'Roll'});
+    % Attitude covariance is for small angle errors (δθ), not full quaternion
+    
+    plotWithCovariance(navTime, pos_error, P, kfInds_mekf.pos, 'Position Error (m)', {'North', 'East', 'Down'});
+    plotWithCovariance(navTime, vel_err, P, kfInds_mekf.vel, 'Velocity Error (m/s)', {'V_N', 'V_E', 'V_D'});
+    plotWithCovariance(navTime, gb_err, P, kfInds_mekf.gyroBias, 'Gyro Bias Estimation (rad/s)', {'X', 'Y', 'Z'});
+    plotWithCovariance(navTime, ab_err, P, kfInds_mekf.accelBias, 'Accel Bias Estimation (m/s^2)', {'X', 'Y', 'Z'});
+    plotWithCovariance(navTime, mb_err, P, kfInds_mekf.magBias, 'Mag Bias Estimation (uT)', {'X', 'Y'});
+    plotWithCovariance(navTime, eul_error, P, [1:3], 'Euler Angle Error (deg)', {'Yaw', 'Pitch', 'Roll'});
+    
+    % the small angle errors (δθ) rather than quaternion errors
+    % plotWithCovariance(navTime, q_err, P, kfInds_mekf.quat, 'Quaternion Error', {'q_w', 'q_x', 'q_y', 'q_z'});
 end
 
 function plotWithCovariance(timeVec, errorVec, P, inds, yLabelStr, labels)
