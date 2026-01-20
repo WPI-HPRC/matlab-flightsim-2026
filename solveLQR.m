d28033e1952943b140d73b3d468adab8f9b91946
Rohan Inamdar <rohan1046yw@gmail.com>
2026-01-20 16:44:03 -0500

    started LQR solver + equation correct for canards.S trapezoidal area

---
+++ solveLQR.m
@@ -0,0 +1,49 @@
+% x = [R P Y, Rdot, Pdot, Ydot]^T
+% u = [delta1, delta2, delta3, delta4]^T where delta is the canard deflection
+% angle
+% d = [alpha, beta]^T for disturbances of angle of attack and sideslip angle
+
+% note this solution of A and B are based on the linearization that cos
+% delta is approx 1. This is for a max deflection of 10 degrees
+
+kins = HPMR_MissileKinematics();
+Jr = kins.I_x;
+Jl = kins.I_y; % Iy and Iz are symmetric
+Aref = kins.S;
+dbody = kins.diameter;
+N = 4; %num fins
+
+gainSched = dictionary;
+vels = 1:2.5:100; % 1 to 100 m/s
+for vel = vels
+    %Cdp = ;% Roll damping moment coeff derivative w.r.t roll rate
+    Cdp = 1;
+    C2 = 1;
+    La = 1;
+    Ma = 1;
+    Na = 1;
+    C1 = 1;
+    
+    
+    A = [0 0 0 1       0        0;...
+         0 0 0 0       1        0;...
+         0 0 0 0       0        1;...
+         0 0 0 -Cdp/Jr 0        0;...
+         0 0 0 0       -C2/Jl   0;...
+         0 0 0 0       0        -C2/Jl];
+    
+    B = [0      0       0       0;...
+         0      0       0       0;...
+         0      0       0       0;...
+         -La/Jr -La/Jr  -La/Jr  -La/Jr;...
+         -Ma/Jl Ma/Jl   0       0;...
+         0      0       -Na/Jl  Na/Jl];
+    
+    E = [0              0;...
+         0              0;...
+         0              0;...
+         0              0;...
+         2*Ma/Jl-C1/Jl  0;...
+         0              2*Na/Jl-C1/Jl];
+end
+
