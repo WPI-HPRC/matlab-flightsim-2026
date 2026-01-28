0a0da633f957f18652fc61ae785f2d081e313a7b
rohan908 <145878828+rohan908@users.noreply.github.com>
2026-01-27 19:53:37 -0500

    added mach number solver, necessary for CNalpha0

--- Plotting/plotSim.m
+++ Plotting/plotSim.m
@@ -7,6 +7,7 @@ LLA = out.LLA.Data;
 v_B = out.V_B.Data;
 mass = out.mass.Data;
 RPY  = rad2deg(out.RPY.Data);
+quatData = angle2quat(out.RPY.Data(3), out.RPY.Data(2), out.RPY.Data(1));
 F_D  = vecnorm(out.F_D.Data');
 
 % Nav Data
@@ -53,6 +54,64 @@ grid on;
 title('Euler Angles');
 ylabel('Angle (deg)');
 xlabel('Time (s)');
+
+% Set up main figure
+dataVis = figure('Name', 'Missile Data Visualization');
+% COLUMN 1: Orientation Plot
+subplot(1,3,1); 
+hold on;
+ax = gca;
+pose = poseplot(ones('quaternion'));
+pose.Orientation = quaternion(quatData.');
+
+grid on;
+axis equal;
+xlabel('X (m)');
+ylabel('Y (m)');
+zlabel('Z (m)');
+title('Missile Orientation');
+
+% COLUMN 2: Altitude & Velocity
+subplot(2,3,2); % Altitude (top half)
+altitudePlot = plot(nan, nan, 'r');
+title('Altitude Vs. Time');
+ylabel('Altitude (m)');
+xlabel('Time (s)');
+grid on;
+hold on;
+
+subplot(2,3,5); % Velocity
+velocityPlot = plot(nan, nan, 'r');
+title('Velocity Vs. Time');
+ylabel('Velocity (m/s)');
+xlabel('Time (s)');
+grid on;
+hold on;
+
+% COLUMN 3: Angular Velocities
+subplot(3,3,3); % Angular Velocity X (Roll Rate)
+omegaXPlot = plot(nan, nan, 'r');
+title('Angular Velocity X (Roll Rate)');
+ylabel('Angular Velocity (rad/s)');
+xlabel('Time (s)');
+grid on;
+hold on;
+
+subplot(3,3,6); % Angular Velocity Y (Pitch Rate)
+omegaYPlot = plot(nan, nan, 'g');
+title('Angular Velocity Y (Pitch Rate)');
+ylabel('Angular Velocity (rad/s)');
+xlabel('Time (s)');
+grid on;
+hold on;
+
+subplot(3,3,9); % Angular Velocity Z (Yaw Rate)
+omegaZPlot = plot(nan, nan, 'b');
+title('Angular Velocity Z (Yaw Rate)');
+ylabel('Angular Velocity (rad/s)');
+xlabel('Time (s)');
+grid on;
+hold on;
 
 %% Position ECI
 
